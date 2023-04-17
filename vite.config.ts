26681157decf02be368c1a25a85ae3fb6a27ce60
Anton Cristo <antoncristo92@gmail.com>
2023-04-17 14:03:21 +0300

    added vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	test: {
+		name: 'ui-unit-tests',
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/tests/setupTests.ts'
+	}
+});
+
+/**
+ import { defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+	viteConfig,
+	defineConfig({
+		test: {
+			name: 'ui-unit-tests',
+			root: './src', // TODO: set to the desired components tree
+			exclude: ['packages/template/*'],
+			globals: true,
+			setupFiles: './src/tests/setupFiles.js',
+			environment: 'jsdom'
+		}
+	})
+);
+
+ */
