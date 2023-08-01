b4ddf3466914776f7aa6c9c7ea3d5db41109a08b
Anton Cristo <antoncristo92@gmail.com>
2023-08-01 15:24:12 +0300

    template cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,8 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import aa from './assets/react.svg';
-import './App.css';
+import { TestComponent } from 'components';
 
-function App() {
-	const [count, setCount] = useState<number>(0);
-
-	const handleCountClick = (): void => {
-		setCount(count => count + 1);
-	};
-
-	return (
-		<div className='App'>
-			<div>
-				<a href='https://vitejs.dev'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://reactjs.org'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={handleCountClick}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-		</div>
-	);
-}
-
-export default App;
+export const App = () => (
+	<div>
+		<h1>Vite App</h1>
+		<TestComponent />
+	</div>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tsconfigPaths()],
 	test: {
 		name: 'ui-unit-tests',
 		globals: true,
 		environment: 'jsdom',
-		setupFiles: './src/tests/setupTests.ts'
+		setupFiles: './src/tests/setup-tests.ts'
 	}
 });
